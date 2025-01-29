286d7cd185c1e47e6723625216988e2227614f18
surajdeveloper2 <surajget248@gmail.com>
2025-01-30 01:06:33 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,10 +8,10 @@ class Config(object):
     TOKEN = "7589023976:AAFerTqtgkPEhmNsAU_5XOFtBaTcd7g-3EE"
     mongo_url = "mongodb+srv://surajgod112:surajgod113@cluster0.v52wo.mongodb.net/"
     PHOTO_URL = ["https://graph.org/file/40d3df88f6d827ff48af5-9e72ee221223e291ed.jpg", "https://graph.org/file/40d3df88f6d827ff48af5-9e72ee221223e291ed.jpg"]
-    SUPPORT_CHAT = "ntpcraj"
-    UPDATE_CHAT = "ntpcdjk"
-    BOT_USERNAME = "Waifugrls17bot"
-    CHARA_CHANNEL_ID = "-1002300918510"
+    SUPPORT_CHAT = "notyourtypeGod"
+    UPDATE_CHAT = "waifubotdjk"
+    BOT_USERNAME = "Waifugrab17bot"
+    CHARA_CHANNEL_ID = "-1002374252364"
     api_id = 29830137
     api_hash = "4fdb6b13530915e7b513ee4318f109b9"
 
