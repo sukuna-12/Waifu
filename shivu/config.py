112b7d1bfd08f454767fcc6953aea4d5a7841761
surajdeveloper2 <surajget248@gmail.com>
2025-02-01 23:00:42 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "6087651372"
     sudo_users = "6087651372", "7667987606", "7378476666", "6858718276"
-    GROUP_ID = -1002316438413
+    GROUP_ID = -1002499806698
     TOKEN = "7785998273:AAHER3KsHxhsZG2JFlA9uUOfeY0qwFW8i1c"
     mongo_url = "mongodb+srv://surajgod112:surajgod113@cluster0.v52wo.mongodb.net/"
     PHOTO_URL = ["https://graph.org/file/09e83a1d89aceabd480c5-2afc46a31083fe23f2.jpg", "https://graph.org/file/0aa659508c1add9ae4c86-2b335aa5262b7b64d2.jpg"]
