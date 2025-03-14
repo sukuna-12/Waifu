7543301553a65b7f067d55527233b8e13bae53ca
surajdeveloper2 <surajget248@gmail.com>
2025-03-14 19:54:36 +0530

    Update transfer.py

--- shivu/modules/transfer.py
+++ shivu/modules/transfer.py
@@ -2,7 +2,7 @@ from telegram.ext import CommandHandler
 from shivu import application, user_collection
 
 # Replace OWNER_ID with the actual owner's user ID
-OWNER_ID = 6087651372
+OWNER_ID = 7640076990
 
 async def transfer(update, context):
     try:
