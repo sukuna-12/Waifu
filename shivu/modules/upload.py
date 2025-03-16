1ea0b7d254475f1fb9e1977c236a621ecd7af49d
surajdeveloper2 <surajget248@gmail.com>
2025-03-16 19:43:49 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -12,7 +12,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪Common), 2 (🟣 Rare) , 3 (🟢 Medium), 4 (🟡 Legendary), 5 (💮 Special Edition), 6 (🔮 Limited Edition), 7 (🎐 Celestial Beauty), 8 (🔖 Cosplay), 9(💦 Wet Elegance), 10(🪽 Divine Edition), 11(💸 Premium Edition), 12(👘 Kimono Grace), 13(🌙 Moonlight Enigma), 14(❄️ Frost Enchantress), 15(🪔 Diwali Radiance), 16(🌈 Holi Color Brust), 17(☪️ Eid Crescent Beauty), 18(💝 Valentine's Beloved), 19(🎭𝐂𝐨𝐬𝐩𝐥𝐚𝐲), 20(🎀𝐒𝐮𝐫𝐩𝐫𝐢𝐬𝐞)"""
+rarity_map = 1 (⚪Common), 2 (🟣 Rare) , 3 (🟢 Medium), 4 (🟡 Legendary), 5 (💮 Special Edition), 6 (🔮 Limited Edition), 7 (🎐 Celestial Beauty), 8 (🔖 Cosplay), 9(💦 Wet Elegance), 10(🪽 Divine Edition), 11(💸 Premium Edition), 12(👘 Kimono Grace), 13(🌙 Moonlight Enigma), 14(❄️ Frost Enchantress), 15(🪔 Diwali Radiance), 16(🌈 Holi Color Brust), 17(☪️ Eid Crescent Beauty), 18(💝 Valentine's Beloved), 19(🛕 Ram Navami Devotion), 20(🎇 New Year's Sparkle), 21(🎃 Halloween Specte), 22(🌲 Christmas Miracle), 23(🌞 Midsummer Bloom)"""
 
 
 
